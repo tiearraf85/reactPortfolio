2529c141ea7addb9fa144cced0733ce2fda22e02
Tiearra Westbrook <tiearraf85@gmail.com>
2025-03-05 14:04:41 -0500

    finished About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,20 @@
+import "./About.css";
+
 export default function About() {
     return (
+        <>
         <div className="about">
             <h1>About Me</h1>
-            <p>Hi, I'm Tiearra Westbrook, a passionate Full Stack Developer with a love for coding and creating innovative solutions. With a background in web development, I specialize in building dynamic and user-friendly applications that deliver exceptional user experiences. My expertise lies in HTML, CSS, JavaScript, and various frameworks and libraries. I thrive on challenges and enjoy collaborating with teams to bring ideas to life. Let's connect and explore how we can work together to make a positive impact through technology.</p>
+            <div className="aboutMe">
+            <p>Hi,</p>
+             <p> I'm Tiearra Westbrook, a passionate Full Stack Developer with a love for coding and creating innovative solutions.
+                 With a background in web development, I specialize in building dynamic and user-friendly applications that deliver
+                  exceptional user experiences. My expertise lies in React, HTML, CSS, JavaScript, and various frameworks and libraries. I 
+                  thrive on challenges and enjoy collaborating with teams to bring ideas to life. Let's connect and explore how we can 
+                  work together to make a positive impact through technology.</p>
         </div>
+        </div>
+
+        </>
     )
 }
