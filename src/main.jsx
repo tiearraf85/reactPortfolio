7a97378677487f789e11d9a03b13cb0f89ad0bf9
Tiearra Westbrook <tiearraf85@gmail.com>
2025-09-18 15:30:52 -0400

    added speed-insight from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 // import './index.css'
 // import App from './App.jsx'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import Nav from './component/Nav.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <App />
   </Router>,
+
+  <SpeedInsights />
 )
